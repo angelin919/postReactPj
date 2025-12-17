99d7bc7052935bb0bedfc498c8c509359d6cb99f
Angelina Ogibenina <74567293+angelin919@users.noreply.github.com>
2025-12-17 19:34:08 +0300

    HW1

---
+++ post-app/src/app/App.tsx
@@ -0,0 +1,35 @@
+import { useEffect, useState } from 'react'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Header from '../widgets/LayoutHeader/Header'
+import Footer from '../widgets/LayoutFooter/Footer'
+import PostCard, { Post } from '../entities/ui/PostCard'
+import PostList from '../widgets/PostList/PostList'
+import MainLayout from '../shared/layouts/MainLayout'
+
+function App() {
+  const [posts, setPosts] = useState<Post[]>([])
+  const [loading, setLoading] = useState(true)
+  useEffect(() => {
+    setTimeout(() => {
+      setPosts([
+        { id: 1, title: 'Первый пост', body: 'Текст первого поста', userId: 1 },
+        { id: 2, title: 'Второй пост', body: 'Текст второго поста', userId: 1 },
+        { id: 3, title: 'Третий пост', body: 'Текст третьего поста', userId: 1 },
+        { id: 4, title: 'Третий пост', body: 'Текст третьего поста', userId: 1 },
+      ])
+      setLoading(false)
+    }, 500)
+  }, [])
+
+  if (loading) {
+    return <div>Загрузкаююю</div>
+  }
+  return (
+    <MainLayout>
+      <PostList posts={posts}/>
+    </MainLayout>
+  )
+}
+
+export default App
